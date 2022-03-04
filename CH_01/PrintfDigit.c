750b766450f153d60d769a553b9ed0ef5ccb4d17
列怒欺 <79437752+872908391@users.noreply.github.com>
2022-03-04 16:53:09 +0800

    输出任意实数

---
+++ CH_01/PrintfDigit.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void PrintfOut(double num, int length);
+
+int main(int argc, char **argv)
+{
+    PrintfOut(7234.2631,3);
+    return 0;
+}
+
+void PrintfOut(double num, int length)
+{
+    // 负数部分
+   if (num < 0)
+   {
+       num = -num;
+       printf("-");
+   }
+   //  正数部分
+   int pos = num;
+   if (pos>=10)
+       PrintfOut(pos/10,length);
+   printf("%d", pos%10);
+   // 小数部分
+   num = num-pos;
+   if(num!=0)
+   {
+       printf(".");
+       for(int i=0; i <length; i++)
+       {
+           num = num*10;
+       }
+       int dec = num;
+       if (dec>=10)
+           PrintfOut(dec/10,length);
+        printf("%d", dec%10);  
+   }
+   
+}
