0fbd237adb65ccd7836bf9b4c24ccf96272dd3a9
列怒欺 <79437752+872908391@users.noreply.github.com>
2022-04-13 16:50:40 +0800

    链表找特定元素递归与非递归

---
+++ CH_03/Find.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<windows.h>
+
+typedef struct Node* ptrToNode;
+typedef ptrToNode List;
+typedef struct Node** Position;
+struct Node {
+	int num;
+	List next;
+};
+
+List CreateNode(int element,Position head,List L);
+List FindNode(int Element, List head);
+List ReFindNode(int Element, List head);
+void PrintNode(List head);
+
+#define NUM 40201
+
+int main(int argc, char** argv)
+{
+    int arr[NUM] = {0};
+    for (int i = 0;i<NUM;i++)
+    {
+        arr[i] = i+1;
+    }
+    List L = malloc(sizeof(struct Node));
+    List head = NULL;
+    
+    //创建两个链表P L
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
+        L = CreateNode(arr[i], &head, L);
+    }
+
+    clock_t start, finish;  
+    double  duration; 
+    start = clock(); 
+    // List pos = FindNode(299999,head); 
+    List pos = ReFindNode(40201,head);
+    if (pos != NULL){
+        printf("%d\n",pos->num);
+    }
+    else{
+        printf("error\n");
+    }
+    finish = clock();  
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;  
+    printf( "%f seconds\n", duration ); 
+    return 0;
+}
+
+//创建节点
+List CreateNode(int element,Position head,List L)
+{
+    List temp = malloc(sizeof(struct Node));
+    temp->num = element;
+    temp->next = NULL;
+    if(*head==NULL)
+    {
+        *head = L =temp;
+    }
+    else
+    {
+        L->next = temp;
+        L = temp;
+    }
+    return L;
+}
+//找到特定元素的节点并返回位置(非递归版)
+List FindNode(int Element, List head)
+{
+    List point = head;
+    while (point != NULL)
+    {
+        if (point->num == Element)
+        {
+            return point;
+        }
+        point = point->next;
+    }
+    return NULL;
+}
+//找到特定元素的节点并返回位置(递归版)
+List ReFindNode(int Element, List head)
+{
+    if(head)
+    {
+        if(head->num==Element)
+        return head;
+        else{
+            head = head->next;
+            return ReFindNode(Element, head);
+        }
+    }
+    return NULL;
+}
+//打印链表
+void PrintNode(List head)
+{
+    List pos = head;
+    while(pos != NULL)
+    {
+        printf("%d\n",pos->num);
+        pos = pos->next;
+    }
+}
---
+++ CH_03/Josephus.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<windows.h>
+
+typedef struct Node* ptrToNode;
+typedef ptrToNode List;
+typedef struct Node** Position;
+struct Node {
+	int num;
+	List next;
+};
+
+void Josephus(int* arr,int length, int N);
+#define NUM 20000
+
+int main(int argc, char **argv)
+{
+    int arr[NUM] = {0};
+    for (int i = 0;i<=NUM;i++)
+    {
+        arr[i] = i+1;
+    }
+    int length = sizeof(arr) / sizeof(arr[0]);
+    clock_t start, finish;  
+    double  duration; 
+    start = clock(); 
+    Josephus(arr,length,1);
+    finish = clock();  
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;  
+    printf( "%f seconds\n", duration ); 
+    return 0;
+}
+//循环链表解决约瑟夫问题 O(MN)
+void Josephus(int* arr,int length, int N)
+{
+    List pos = malloc(sizeof(struct Node));
+    pos->num = arr[0];
+    pos->next = pos;
+    for (int i = 1; i < length; i++)
+    {
+        List temp = malloc(sizeof(struct Node));
+        temp->num = arr[i];
+        temp->next = pos->next;
+        pos->next = temp;
+        pos = temp;
+    }
+    int total = length-1;
+    pos = pos->next;
+    while(total--)
+    {
+        for(int i = 0; i <N-1;i++ )
+        {
+            pos = pos->next;
+        }
+        List temp = pos->next;
+        pos->next = pos->next->next;
+        pos = pos->next;
+        free(temp);
+    }
+    printf("%d\n",pos->num);
+    free(pos);
+}
