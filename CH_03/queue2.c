1e7102247ff30f35f9c72dec63716e79c64b3c18
列怒欺 <79437752+872908391@users.noreply.github.com>
2022-05-28 20:34:50 +0800

    数组队列

--- CH_03/queue2.c
+++ CH_03/queue2.c
@@ -13,6 +13,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define FrontInit 0;
+#define RearInit 0;
 typedef int ElementType;
 typedef struct _queue queue;
 typedef queue* Queue; 
@@ -21,10 +23,102 @@ struct _queue{
     int Capacity;
     int front;
     int rear;
+    int size;
     ElementType* Array;
 };
+Queue EmptyQueue(int MaxQueueSize);
+bool Enqueue(Queue queue,ElementType element);
+bool Dequeue(Queue queue);
+ElementType Front(Queue queue);
+bool IsEmpty(Queue queue);
+bool IsFull(Queue queue);
+void MakeEmpty(Queue queue);
+void ClearQueue(Queue queue);
+
 
 int main(int argc, char **argv)
 {
+    Queue queue = EmptyQueue(10);
+    Enqueue(queue,11);
+    Enqueue(queue,2);
+    Enqueue(queue,3);
+    printf("%d\n",Front(queue));
+    Dequeue(queue);
+    printf("%d\n",Front(queue));
+    Dequeue(queue);
+    printf("%d\n",Front(queue));
+    Dequeue(queue);
+    Dequeue(queue);
+    ClearQueue(queue);
     return 0;
+}
+
+// create a queue of a array
+Queue EmptyQueue(int MaxQueueSize)
+{
+    Queue queue = malloc(sizeof(queue));
+    // queue->Array = malloc(sizeof(ElementType)*(MaxQueueSize+1));
+    queue->Array = malloc(sizeof(ElementType)*(MaxQueueSize));
+    queue->Capacity = MaxQueueSize;
+    MakeEmpty(queue);
+    return queue;
+}
+// insert an element from the queue of a array
+bool Enqueue(Queue queue,ElementType element)
+{
+    if(IsFull(queue)) 
+    {
+        printf("the queue is already empty\n");
+        return false;
+    }
+    else 
+    {
+        queue->Array[queue->rear] = element;
+        queue->rear = queue->rear + 1>=queue->Capacity? 0 : queue->rear+1;
+        queue->size++;
+    }
+    return true;
+}
+// out an element from the queue of a array
+bool Dequeue(Queue queue)
+{
+    if(IsEmpty(queue))
+    {
+        printf("the queue is already empty\n");
+        return false;
+    }
+    else
+    {
+        queue->front = queue->front+1>=queue->Capacity? 0 : queue->front+1;
+        queue->size--;
+    } 
+    return true;
+}
+// display the front element of the queue
+ElementType Front(Queue queue)
+{
+    return queue->Array[queue->front];
+}
+// check if the queue is empty
+bool IsEmpty(Queue queue)
+{
+    return queue->size==0;
+}
+// check if the queue is full
+bool IsFull(Queue queue)
+{
+    return queue->size==queue->Capacity;
+}
+// empty the queue
+void MakeEmpty(Queue queue)
+{
+    queue->front = FrontInit;
+    queue->rear = RearInit;
+    queue->size = 0;
+}
+// delete the queue
+void ClearQueue(Queue queue)
+{
+    free(queue->Array);
+    free(queue);
 }
