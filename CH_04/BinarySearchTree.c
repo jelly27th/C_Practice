f95dfaf65626fa880bcd134225a24bb7037c2a98
列怒欺 <79437752+872908391@users.noreply.github.com>
2022-06-08 13:55:43 +0800

    含有两儿子节点删除的四种方法

--- CH_04/BinarySearchTree.c
+++ CH_04/BinarySearchTree.c
@@ -1,7 +1,7 @@
 /**
  * @file BinarySearchTree.c
  * @author Jelly (wugd827@163.com)
- * @brief Exercise 4.10
+ * @brief Exercise 4.10 and Exercise 4.13
  * @version 0.1
  * @date 2022-06-04
  * 
@@ -12,6 +12,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
+#include <windows.h>
 
 typedef int ElementType;
 typedef struct _treeNode TreeNode;
@@ -29,8 +31,12 @@ Position Find(SearchTree T, ElementType element);
 Position FindMin(SearchTree T);
 Position FindMax(SearchTree T);
 SearchTreeAddress Insert(SearchTreeAddress T,ElementType element);
-SearchTree Delete(SearchTree T,ElementType element);
+SearchTree DeleteRight(SearchTree T,ElementType element);
 SearchTree Preorder(SearchTree T);
+SearchTree DeleteLeft(SearchTree T,ElementType element);
+void AlternateDelete(SearchTree T, ElementType element);
+bool Rand();
+void RandomDelete(SearchTree T, ElementType element);
 
 int main(int argc, char** argv)
 {
@@ -41,10 +47,16 @@ int main(int argc, char** argv)
   {
     Insert(&T,arr[i]);
   }
-  printf("%d\n",Find(T,61)->data);
-  printf("%d\n",FindMin(T)->data);
-  printf("%d\n",FindMax(T)->data);
-  Delete(T,61);
+  // printf("%d\n",Find(T,61)->data);
+  // printf("%d\n",FindMin(T)->data);
+  // printf("%d\n",FindMax(T)->data);
+  // DeleteRight(T,61);
+  // DeleteLeft(T,75);
+  // AlternateDelete(T,61);
+  // AlternateDelete(T,75);
+  RandomDelete(T,61);
+  RandomDelete(T,75);
+  RandomDelete(T,53);
   Preorder(T);
   T = MakeEmpty(T);
   printf("%p\n",T);
@@ -89,7 +101,7 @@ Position FindMax(SearchTree T)
 {
     if(T==NULL)//when the point is null
       return NULL;
-    while(T->right==NULL)
+    while(T->right!=NULL)
     {
         T = T->right;//if not find the maximun element,go right
     }
@@ -111,21 +123,21 @@ SearchTreeAddress Insert(SearchTreeAddress T,ElementType element)
       return Insert(&(*T)->right,element);
     return T;//insert completed successfully
 }
-// find the element at the tree and delete it
-SearchTree Delete(SearchTree T,ElementType element)
+// find the element at the tree and delete it(replace it with a right subtree minimum)
+SearchTree DeleteRight(SearchTree T,ElementType element)
 {
     Position temp = NULL;
     if(T==NULL)//when the point is null
       return NULL;
     else if(element<T->data)//go left
-      T->left =  Delete(T->left, element);
+      T->left =  DeleteRight(T->left, element);
     else if(element>T->data)//go Right
-      T->right =  Delete(T->right, element);
+      T->right =  DeleteRight(T->right, element);
     else if(T->right&&T->left)//have two children
     {
         temp = FindMin(T->right);
         T->data = temp->data;
-        T->right =  Delete(T->right,temp->data);
+        T->right =  DeleteRight(T->right,temp->data);
     }
     else //have one or zero children
     {
@@ -146,4 +158,53 @@ SearchTree Preorder(SearchTree T)
   Preorder(T->left);
   Preorder(T->right);
   return T;
+}
+// find the element at the tree and delete it(replace it with a left subtree maximun)
+SearchTree DeleteLeft(SearchTree T,ElementType element)
+{
+  Position temp =  NULL;
+  if(T==NULL)
+    return NULL;
+  else if(element<T->data)
+    T->left = DeleteLeft(T->left, element);
+  else if(element>T->data)
+    T->right = DeleteLeft(T->right, element);
+  else if(T->left&&T->right)//have two children
+  {
+    temp = FindMax(T->left);
+    T->data = temp->data;
+    T->left = DeleteLeft(T->left, temp->data);
+  }
+  else //have one or zero children
+  {
+    temp = T;
+    if(T->left==NULL) T = T->right;
+    else if(T->right==NULL) T = T->left;
+    free(temp);
+  }
+  return T;
+}
+// alternately use DeleteLeft() and DeleteRight() to delete element
+void AlternateDelete(SearchTree T, ElementType element)
+{
+  static bool flag = true;
+  if(flag) DeleteLeft(T, element);
+  else DeleteRight(T, element);
+  flag = !flag;
+}
+// hardware delay generation the random number
+bool Rand()
+{
+  srand((unsigned)time(NULL));
+  Sleep(1000);//delay 1S
+  return rand() % 2;//scope is 0~1
+}
+// random use DeleteLeft() and DeleteRight() to delete element
+void RandomDelete(SearchTree T, ElementType element)
+{
+  static bool flag = true;
+  flag = Rand();
+  printf("%d\n",flag);
+  if(flag==true) DeleteLeft(T, element);
+  else DeleteRight(T, element);
 }
