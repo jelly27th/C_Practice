529d75cfa88dd1096a65527e0aebc41821b068d0
Jelly <872908391@qq.com>
2022-09-12 20:02:11 +0800

    堆其他操作

--- test/BinHeapTest.c
+++ test/BinHeapTest.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv)
 {
-    int arr[] = {10,12,1,14,6,5,8,15,3,9,7,4,11,13,2};
+    // int arr[] = {10,12,1,14,6,5,8,15,3,9,7,4,11,13,2};
     int arr1[] = {150,80,40,30,10,70,110,100,20,90,60,50,120,140,130};
     BinHeap H = BinHeapInit(15);
 
@@ -15,16 +15,15 @@ int main(int argc, char **argv)
     // BinHeapDeleteMin(H,1);
     // BinHeapPrint(H); 
     
-    for(int i=0;i<sizeof(arr)/sizeof(arr[0]); i++)
-      H->Elements[i] = arr[i];
+    for(int i=1;i<=sizeof(arr1)/sizeof(arr1[0]); i++)
+      H->Elements[i] = arr1[i-1];
     H->Size = 15;
+    // BinHeapPrint(H);
     BinHeapBuildHeap(H); 
     BinHeapPrint(H);
-    BinHeapIncreaseKey(H,15,2);
+    BinHeapIncreaseKey(H,15,2); 
     BinHeapPrint(H);
     BinHeapDecraseKey(H,25,2);
-    BinHeapPrint(H);
-    BinHeapDecraseKey(H,NONE,1);
     BinHeapPrint(H);
     BinHeapDelete(H,3);
     BinHeapPrint(H);
