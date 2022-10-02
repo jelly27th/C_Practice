e3fcb8990825eb35cec18e9ba3b0af4c321ca8e5
Jelly <872908391@qq.com>
2022-10-02 18:41:43 +0800

    斜堆（未测试）

---
+++ include/SkewHeap.h
@@ -0,0 +1,17 @@
+#ifndef _SKEWHEAP_H
+#define _SKEWHEAP_H
+
+typedef int SkewHeap_ElementType;
+typedef struct _SkewHeapNode SkewHeapNode;
+typedef SkewHeapNode* SkewHeap;
+
+struct _SkewHeapNode{
+    SkewHeap_ElementType element;
+    SkewHeap Right;
+    SkewHeap Left;
+};
+
+SkewHeap SkewHeap_Init(SkewHeap_ElementType element);
+SkewHeap SkewHeap_Merge(SkewHeap H1, SkewHeap H2);
+
+#endif
---
+++ src/SkewHeap.c
@@ -0,0 +1,43 @@
+/**
+ * @file SkewHeap.c
+ * @author Jelly (wugd827@163.com)
+ * @brief I'm not doing tests for now, maybe not in the future
+ * @version 0.1
+ * @date 2022-10-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <SkewHeap.h>
+#include <stdlib.h>
+
+SkewHeap SkewHeap_Init(SkewHeap_ElementType element)
+{
+    SkewHeap H = malloc(sizeof(SkewHeapNode));
+    H->Left = H->Right = NULL;
+    H->element = element;
+
+    return H;
+}
+
+SkewHeap SkewHeap_Merge(SkewHeap H1, SkewHeap H2)
+{
+    if(H1==NULL) return H2;
+    if(H2==NULL) return H1;
+
+    if(H1->element>H2->element)
+       Swap(H1,H2);
+    
+    Swap(H1->Left,H2->Right);
+
+    H1->Left = SkewHeap_Merge(H2,H1->Left);
+
+    return H1;
+}
+
+static void Swap(SkewHeap H1, SkewHeap H2)
+{
+    SkewHeap temp = H1;
+    H1 = H2;
+    H2 = temp;
+}
