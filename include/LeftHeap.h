7dfdf3ff533c3fef9ef0065b37d72298d34b4d77
Jelly <872908391@qq.com>
2022-10-02 10:47:17 +0800

    左堆（未测试）

---
+++ include/LeftHeap.h
@@ -0,0 +1,24 @@
+#ifndef _LEFTHEAP_H
+#define _LEFTHEAP_H
+#include <stdbool.h>
+
+typedef int Leftheap_element_type;
+typedef struct _LeftHeapNode LeftHeapNode;
+typedef LeftHeapNode* Leftheap;
+struct _LeftHeapNode{
+    Leftheap_element_type element;
+    Leftheap Left;
+    Leftheap Right;
+    int NPL;
+};
+
+Leftheap Leftheap_Init(Leftheap_element_type element);
+Leftheap_element_type Leftheap_FinMin(Leftheap H);
+bool Leftheap_IsEmpty(Leftheap H);
+Leftheap Leftheap_Merge(Leftheap H1, Leftheap H2);
+#define Leftheap_Insert(X,H) (H = Leftheap_Insert1((X),H))
+Leftheap Leftheap_Insert1(Leftheap_element_type X, Leftheap H);
+#define Leftheap_DeleteMin(H) (H = Leftheap_DeleteMin1((H)));
+Leftheap Leftheap_DeleteMin1(Leftheap H);
+
+#endif
---
+++ src/LeftHeap.c
@@ -0,0 +1,89 @@
+/**
+ * @file LeftHeap.c
+ * @author Jelly (wugd827@163.com)
+ * @brief I'm not doing tests for now, maybe not in the future
+ * @version 0.1
+ * @date 2022-10-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <Leftheap.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+Leftheap Leftheap_Init(Leftheap_element_type element)
+{
+    Leftheap H = malloc(sizeof(LeftHeapNode));
+    H->Left = NULL;
+    H->Right = NULL;
+    H->NPL = 0;
+    H->element = element;
+
+    return H;
+}
+// the root of left heap is minimum value 
+Leftheap_element_type Leftheap_FinMin(Leftheap H)
+{
+    return H->element;
+}
+
+bool Leftheap_IsEmpty(Leftheap H)
+{
+    return H==NULL;
+}
+
+Leftheap Leftheap_Merge(Leftheap H1, Leftheap H2)
+{
+    if(H1==NULL) return H2;
+    if(H2==NULL) return H1;
+    if(H1->element<H2->element)
+      return Leftheap_Merge1(H1,H2);
+    else 
+      return Leftheap_Merge1(H2,H1);
+}
+
+static Leftheap Leftheap_Merge1(Leftheap H1, Leftheap H2)
+{
+    if(H1->Left == NULL)
+      H1->Left = H2;
+    else
+    {
+        H1->Right = Leftheap_Merge(H1->Right,H2);
+        if(H1->Left->NPL<H1->Right->NPL)
+          SWapChildren(H1);
+        
+        H1->NPL = H1->Right->NPL + 1;
+    }
+    return H1;
+}
+
+static void SWapChildren(Leftheap H)
+{
+    Leftheap temp = H->Left;
+    H->Left = H->Right;
+    H->Right = temp;
+}
+
+Leftheap Leftheap_Insert1(Leftheap_element_type X, Leftheap H)
+{
+    Leftheap SingelNode =  Leftheap_Init(X);
+    H = Leftheap_Merge(SingelNode, H);
+    return H;
+}
+
+Leftheap Leftheap_DeleteMin1(Leftheap H)
+{
+    Leftheap Left, Right;
+    if(Leftheap_IsEmpty(H))
+    {
+        printf("Leftheap is empty\n");
+        return H;
+    }
+
+    Left = H->Left;
+    Right = H->Right;
+    free(H);
+    return Leftheap_Merge(Left, Right);
+}
+
